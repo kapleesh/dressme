762125b5735d15fce9523d4953f6bfcd9f0b5c97
Jayanth Sundaresan <empoleon5000@yahoo.com>
2016-11-12 04:53:23 -0800

    Set registration routes

--- dressme/wardrobe/views.py
+++ dressme/wardrobe/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+
+class RegisterFormView(generic.TemplateView):
+    template_name = 'wardrobe/register.html'

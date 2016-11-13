e5bed9f997c56a153933df8e1769fb036c4aae7d
Cibi Pari <cibi.pari@berkeley.edu>
2016-11-12 23:42:46 -0800

    Calendar Integrated

--- dressme/clothing/urls.py
+++ dressme/clothing/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url, include
-from clothing.views import user_login, index, register, logout_view
+from clothing.views import *
 
 urlpatterns = [
     url(r'^$', index),
     url(r'^user_login/$', user_login, name='user_login'),
     url(r'^register/$', register, name='register'),
-    url(r'^logout/$', logout_view, name='logout_view')
+    url(r'^authorize/$', authorize, name='authorize'),
+    url(r'^calendarHelper/$', calendarHelper, name='calendarHelper'),
+    url(r'^calendar/$', calendar, name='calendar'),
+    url(r'^logout/$', logout_view, name='logout_view'), 
+    # url(r'^eventAdder/$', eventAdder, name='eventAdder'), 
+    # url(r'^eventAuth/$', eventAuth, name='eventAuth')
 ]

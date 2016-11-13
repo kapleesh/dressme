f1aca1b2d521efb71b7b011399aff8f8bdbbcf7b
Cibi Pari <cibi.pari@berkeley.edu>
2016-11-12 20:11:11 -0800

    Fixed city and state instances

--- dressme/clothing/models.py
+++ dressme/clothing/models.py
@@ -26,9 +26,8 @@ class Wardrobe(models.Model):
 		(8, "HOODIE"),
 		(9, "SWEATER"),
 		(10, "EARMUFFS"),
-		(11, "UMBRELLA"),
-		(12, "SCARF"),
-		(13, "GLOVES"),
+		(11, "SCARF"),
+		(12, "GLOVES"),
 		)
 	CLOTH_WEATHER = (
 		(1, "Cold"),
@@ -37,7 +36,12 @@ class Wardrobe(models.Model):
 		(4, "Warm"),
 		(5, "Hot"))
 
-	inLaundry = models.BooleanField(default = False)
+	in_laundry = models.BooleanField(default = False)
+	for_cold = models.BooleanField(default = False)
+	for_chilly = models.BooleanField(default = False)
+	for_mild = models.BooleanField(default = False)
+	for_warm = models.BooleanField(default = False)
+	for_hot = models.BooleanField(default = False)
 	daysWornBeforeWash = models.IntegerField(default = 0)
 	daysNotUsed = models.IntegerField(default = 0)
 	cloth_type = models.IntegerField(choices = CLOTH_TYPES)
@@ -51,9 +55,9 @@ class Wardrobe(models.Model):
 		return cloth_names_dict.values()
 		
 class StormChaser(User):
-	city = models.CharField(max_length = 30)
-	state = models.CharField(max_length = 2)
-	#wardrobes = models.OneToOneField(Wardrobe)
+	city = models.CharField(max_length = 30, blank=True)
+	state = models.CharField(max_length = 2, blank=True)
+	wardrobe = models.OneToOneField(Wardrobe)
 	
 
 	def getOutfits(self):
@@ -67,7 +71,8 @@ class StormChaser(User):
 		five_day_forecast = data_dict.get('list')
 		one_day_forecast = five_day_forecast[0:7]
 		worstWeather = getWorstWeather(one_day_forecast)
-		weatherUtility(getAvgTemp(one_day_forecast))
+		temp = getAvgTemp(one_day_forecast)
+		return self.weatherTempUtility(temp, worstWeather)
 
 	def getWorstWeather(day_forecasts):
 		weathers = set([day_forecasts[i].get('weather').get('description') for i in range(7)])
@@ -79,9 +84,41 @@ class StormChaser(User):
 	def eventUtility(self):
 		pass
 
-	def temperatureUtility(self, temperature):
-		if temperature <= 55: 
-			possibleClothes = StormChaser.objects.filter(cloth_weather = 1)
+	def temperatureUtility(self, temperature, weather):
+		def getAllClothTypes(possible_clothes):			
+			tops = possible_clothes.objects.filter(cloth_type=1)
+			bottoms = possible_clothes.objects.filter(cloth_type=2)
+			jackets = possible_clothes.objects.filter(cloth_type=3)
+			accessories = possible_clothes.objects.filter(cloth_type=4)
+			return {"tops": tops, "bottoms": bottoms, "jackets": jackets, "accessories": accessories}
+
+
+		weather_type = 0
+
+		if temperature <= 55:
+			possible_clothes = wardrobe.objects.filter(for_cold=True, in_laundry=False)
+			weather_type = 1
+		elif temperature <= 68:
+			possible_clothes = wardrobe.objects.filter(for_chilly=True, in_laundry=False)
+			weather_type = 2
+		elif temperature <= 78:
+			possible_clothes = wardrobe.objects.filter(for_mild=True, in_laundry=False)
+			weather_type = 3
+		elif temperature <= 85:
+			possible_clothes = wardrobe.objects.filter(for_warm=True, in_laundry=False)
+			weather_type = 4
+		else: 
+			possible_clothes = wardrobe.objects.filter(for_hot=True, in_laundry=False)
+			weather_type = 5
+
+		
+		organized_possible = getAllClothTypes(possible_clothes)
+
+
+
+
+
+
 
 	def weatherUtility(self, weather):
 		pass
